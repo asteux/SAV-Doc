e08933c38495dd9319602ed9157e950a7301ac17
Oumar Traore <oumar.traore.dev@gmail.com>
2021-07-13 10:37:10 +0200

    set networks in truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,30 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+require('dotenv').config();
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const MNEMONIC_PHRASE = process.env.MNEMONIC_PHRASE;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
-    }
-  }
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(MNEMONIC_PHRASE, `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`),
+      network_id: 3,
+      gas: 5500000,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(MNEMONIC_PHRASE, `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`),
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
+  },
 };
