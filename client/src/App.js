8aa7412c83ffab11ead94109f4b31f4075361a27
Oumar Traore <oumar.traore.dev@gmail.com>
2021-07-14 13:22:33 +0200

    client: add web3Slice

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect, Route, Switch } from 'react-router';
+
+import { loadWeb3, updateAccounts } from './common/web3/web3Slice';
 
 import "./App.css";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const web3 = useSelector((state) => state.web3.web3);
+
+  useEffect(() => {
+    // Load Web3
+    dispatch(loadWeb3());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (window.ethereum && web3) {
+      // Manage Metamask Events
+      window.ethereum.on('accountsChanged', async (newAccounts) => {
+        const accounts = await web3.eth.getAccounts();
+        dispatch(updateAccounts(accounts));
+      });
+
+      window.ethereum.on('chainChanged', (chainId) => {
+        window.location.reload();
+      });
+    }
+  }, [dispatch, web3]);
+
+  if (!web3) {
+    return (
+      <div>Loading Web3, accounts, and contract...</div>
+    );
+  }
+
   return (
     <Switch>
       <Route exact path="/" render={() => (<div>Home</div>)} />
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,14 +2,23 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 
+import web3Reducer from '../common/web3/web3Slice';
+
 export const history = createBrowserHistory();
 
 export default configureStore({
   reducer: {
     router: connectRouter(history),
+    web3: web3Reducer,
   },
   middleware: [
-    ...getDefaultMiddleware(),
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          'web3/loadWeb3',
+        ],
+      }
+    }),
     routerMiddleware(history),
   ],
 });
